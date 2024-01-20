d3fdfb5ce85b60709d9d5a58a334fd44ce9a2631
Marcelo Semerdjian <123202844+marcelosemerdjian@users.noreply.github.com>
2024-01-20 11:54:49 -0300

    Create do-while-structure.java

---
+++ do-while-structure.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		char anw;
+		
+		do {
+			System.out.print("Enter the temperature in Celsius:");
+			double C = sc.nextDouble();
+			double F = 9.0 * C / 5.0 + 32.0;
+			System.out.printf("Farenheit equivalent: %.1f%n", F);
+			System.out.print("Do you want to enter a new temp? (y/n)");
+			anw = sc.next().charAt(0);		
+		} while (anw != 'n');
+		
+		
+		sc.close();
+	}
+}
