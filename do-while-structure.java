bc719021926a7977d921e02a2e7cf3027e489e99
Marcelo Semerdjian <123202844+marcelosemerdjian@users.noreply.github.com>
2024-01-20 11:55:21 -0300

    Update do-while-structure.java

--- do-while-structure.java
+++ do-while-structure.java
@@ -10,14 +10,16 @@ public class Main {
 		
 		do {
 			System.out.print("Enter the temperature in Celsius:");
+			
 			double C = sc.nextDouble();
 			double F = 9.0 * C / 5.0 + 32.0;
+			
 			System.out.printf("Farenheit equivalent: %.1f%n", F);
 			System.out.print("Do you want to enter a new temp? (y/n)");
+			
 			anw = sc.next().charAt(0);		
 		} while (anw != 'n');
-		
-		
+
 		sc.close();
 	}
 }
